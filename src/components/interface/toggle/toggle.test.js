379952e56bb3c16e43182bdcc92d045093a8678c
Fredrik Omstedt <fredrik.omstedt@gmail.com>
2021-06-26 17:59:34 +0200

    Added some tests

---
+++ src/components/interface/toggle/toggle.test.js
@@ -0,0 +1,47 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Toggle from './toggle';
+
+const value = false;
+const leftText = 'Chips';
+const rightText = 'Popcorn';
+const setValue = jest.fn();
+
+test('renders leftText', () => {
+    render(
+        <Toggle
+            leftText={leftText}
+            rightText={rightText}
+            value={value}
+            onChange={setValue}
+        />
+    );
+    const toggleElement = screen.getByText(leftText);
+    expect(toggleElement).toBeInTheDocument();
+});
+
+test('renders rightText', () => {
+    render(
+        <Toggle
+            leftText={leftText}
+            rightText={rightText}
+            value={value}
+            onChange={setValue}
+        />
+    );
+    const toggleElement = screen.getByText(rightText);
+    expect(toggleElement).toBeInTheDocument();
+});
+
+test('renders Switch', () => {
+    render(
+        <Toggle
+            leftText={leftText}
+            rightText={rightText}
+            value={value}
+            onChange={setValue}
+        />
+    );
+    const toggleElement = screen.getByRole('switch');
+    expect(toggleElement).toBeInTheDocument();
+});
---
+++ src/utils/utilityFunctions.test.js
@@ -0,0 +1,19 @@
+import { getPriceFromDistance } from './utilityFunctions';
+
+const distance = 10000; // In meters
+const costPerMile = 18.5;
+
+test('returns 0 if distance is 0', () => {
+    const result = getPriceFromDistance(0, costPerMile);
+    expect(result).toBe(0);
+});
+
+test('returns 0 if costPerMile is 0', () => {
+    const result = getPriceFromDistance(distance, 0);
+    expect(result).toBe(0);
+});
+
+test('returns correct cost', () => {
+    const result = getPriceFromDistance(distance, costPerMile);
+    expect(result).toBe(costPerMile);
+});
