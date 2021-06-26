39bc3cfbae66e0ee27f653a15bc39cb5a5d53885
Fredrik Omstedt <fredrik.omstedt@gmail.com>
2021-06-26 16:45:22 +0200

    Styled the toggle switch
    Desktop functionality is now finished :D

--- src/components/interface/priceFromMapCalculator/priceFromMapCalculator.js
+++ src/components/interface/priceFromMapCalculator/priceFromMapCalculator.js
@@ -3,6 +3,10 @@ import CostVisualizer from 'components/interface/costVisualizer/costVisualizer';
 import InfoText from 'components/interface/infoText/infoText';
 import styles from './priceFromMapCalculator.module.css';
 
+const SelectionText = (props) => (
+    <div className={styles.selectionText}>{props.text}</div>
+);
+
 const PriceFromMapCalculator = (props) => {
     const { cost, isRoundTrip, setIsRoundTrip } = props;
     return (
@@ -11,9 +15,23 @@ const PriceFromMapCalculator = (props) => {
                 className={styles.infoText}
                 text={'🗺️ Use the map to select a destination'}
             />
+            {!cost && <div />}
             {cost && <CostVisualizer cost={cost} />}
-            <div>
-                <Switch onChange={setIsRoundTrip} checked={isRoundTrip} />
+            <div className={styles.toggleWrapper}>
+                <SelectionText text={'One way'} />
+                <Switch
+                    onChange={setIsRoundTrip}
+                    checked={isRoundTrip}
+                    offColor={'#428a7c'}
+                    offHandleColor={'#8cd3c4'}
+                    onColor={'#428a7c'}
+                    onHandleColor={'#8cd3c4'}
+                    uncheckedIcon={false}
+                    checkedIcon={false}
+                    height={20}
+                    handleDiameter={30}
+                />
+                <SelectionText text={'Round trip'} />
             </div>
         </div>
     );
