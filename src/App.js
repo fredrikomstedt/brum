f3ee228d8fb60bcaf80ece6c329c3ecaaf4ae528
Fredrik Omstedt <fredrik.omstedt@gmail.com>
2023-05-04 20:22:13 +0200

    Updated cost according to new swedish tax regulations

--- src/App.js
+++ src/App.js
@@ -12,18 +12,18 @@ const App = () => {
             <Interface
                 cost={cost}
                 mapError={mapError}
-                costPerMile={18.5}
+                costPerMile={25}
                 isRoundTrip={isRoundTrip}
                 setIsRoundTrip={setIsRoundTrip}
             />
             <Map
                 home={{
-                    lng: 17.967610,
-                    lat: 59.362570,
+                    lng: 17.96761,
+                    lat: 59.36257,
                     address: 'Brunnsgatan 2A, 172 68 Sundbyberg, Sweden',
                 }}
                 zoom={14}
-                costPerMile={18.5}
+                costPerMile={25}
                 isRoundTrip={isRoundTrip}
                 setCost={setCost}
                 setMapError={setMapError}
--- src/utils/utilityFunctions.test.js
+++ src/utils/utilityFunctions.test.js
@@ -1,7 +1,7 @@
 import { getPriceFromDistance } from './utilityFunctions';
 
 const distance = 10000; // In meters
-const costPerMile = 18.5;
+const costPerMile = 25;
 
 test('returns 0 if distance is 0', () => {
     const result = getPriceFromDistance(0, costPerMile);
